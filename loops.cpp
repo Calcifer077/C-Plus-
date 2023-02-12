b6097b1ce5ca136ada8e883e3bda6b276ce57074
Calcifer077 <115633814+Calcifer077@users.noreply.github.com>
2023-02-12 19:37:53 +0530

    Create loops.cpp

---
+++ loops.cpp
@@ -0,0 +1,28 @@
+//printing hello world 10 times
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	cout<<"Printing hello world ten times \n";
+	for(int i = 0;i<10;i++){
+		cout<<"Hello world \n";
+	}
+	return 0;
+}
+.......>
+//Sum of first 10 integers
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	cout<<"Sum of first 10 numbers \n";
+	int sum =0;
+	for(int i = 0;i<10;i++){
+		sum += i;
+	}
+	cout<<sum;
+	return 0;
+}
+......>
