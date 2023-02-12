9d8f0286bcd0450f12bddc56004e5b02bbdf38c0
Calcifer077 <115633814+Calcifer077@users.noreply.github.com>
2023-02-12 18:35:16 +0530

    Create Basics.cpp

---
+++ Basics.cpp
@@ -0,0 +1,20 @@
+//Datatypes
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int a;//declaration of variables.
+	cout<<"Size of int is "<<sizeof(a)<<endl;
+	
+	float b;
+	cout<<"Size of float is "<<sizeof(b)<<endl;
+	
+	char c;
+	cout<<"Size of char is "<<sizeof(c)<<endl;
+	
+	bool d;
+	cout<<"Size of bool is "<<sizeof(d)<<endl;
+	return 0;
+}
+......>
