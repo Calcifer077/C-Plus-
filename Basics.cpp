7627fcc875a4eb85d194088f2123517016b2a213
Calcifer077 <115633814+Calcifer077@users.noreply.github.com>
2023-02-12 19:01:03 +0530

    Update Basics.cpp

--- Basics.cpp
+++ Basics.cpp
@@ -15,6 +15,49 @@ int main(){
 	
 	bool d;
 	cout<<"Size of bool is "<<sizeof(d)<<endl;
+	
+	short int si;
+	cout<<"Size of short int is "<<sizeof(si)<<endl;
+	
+	long int li;
+	cout<<"Size of long int is "<<sizeof(li)<<endl;
 	return 0;
 }
 ......>
+//using simple input output
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int a,b;
+	cout<<"enter the value of a: ";
+	cin>>a;
+	cout<<"Enter the value of b: ";
+	cin>>b;
+	
+	cout<<"Total value is "<<a+b;
+	return 0;
+}
+.......>
+//using simple if else condition
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int a;
+	cout<<"enter value";
+	cin>>a;
+	
+	if(a>18){
+		cout<<"Eligible to vote. \n";
+		cout<<"Eligible to drive.";
+	}
+	else{
+		cout<<"Not eligible to vote. \n";
+		cout<<"Not eligible to drive.";
+	}
+	return 0;
+}
+.......>
