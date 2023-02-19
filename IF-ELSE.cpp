1f0ccb43adf409bd928fea0726da2a23b21a1f57
Calcifer077 <115633814+Calcifer077@users.noreply.github.com>
2023-02-19 19:20:58 +0530

    Update IF-ELSE.cpp

--- IF-ELSE.cpp
+++ IF-ELSE.cpp
@@ -52,3 +52,37 @@ int main(){
 	return 0;
 }
 ........>
+//switch statement:
+#include<iostream>
+using namespace std;
+int main(){
+	int a;
+	cout<<"INput the character:";
+	cin>>a;
+	switch(a){
+		case 1:
+			cout<<"MOnday";
+			break;
+		case 2:
+			cout<<"Tuesday";
+			break;
+		case 3:
+			cout<<"Wednesday";
+			break;
+		case 4:
+			cout<<"Thurusday";
+			break;
+		case 5:
+			cout<<"Friday";
+			break;
+		case 6:
+			cout<<"Saturday";
+			break;
+		case 7:
+			cout<<"Sunday";
+			break;
+		default:
+			cout<<"Nothing";
+	}
+	return 0;
+}
