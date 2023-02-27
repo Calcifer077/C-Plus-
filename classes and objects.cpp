46a3dfe8abffaae67e560dc2f1afdaa3e9358c68
Calcifer077 <115633814+Calcifer077@users.noreply.github.com>
2023-02-27 19:15:23 +0530

    Update classes and objects.cpp

--- classes and objects.cpp
+++ classes and objects.cpp
@@ -97,3 +97,27 @@ int main(){
 	cout<<"\nAge of employ 2:"<<obj2.age;
 	return 0;
 }
+........>
+#include<iostream>
+
+using namespace std;
+
+class complex{
+	int a,b;
+	public:
+		void setnumber(int n1, int n2){
+			a = n1;
+			b = n2;
+		}
+		void display(){
+			cout<<"Your number is "<<a<<" + "<<b<<"i";
+		}
+};
+int main(){
+	complex c1, c2;
+	c1.setnumber(1,5);
+	c2.setnumber(5,6);
+	c1.display();
+	c2.display();
+	return 0;
+}
