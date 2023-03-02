778dc90710874e22ca03ffa44048f112c4e2f230
Calcifer077 <115633814+Calcifer077@users.noreply.github.com>
2023-03-02 18:47:03 +0530

    Update constructors.cpp

--- constructors.cpp
+++ constructors.cpp
@@ -32,3 +32,88 @@ int main(){
 4. It can have default arguments.
 5. We cannot refer to their addrress.
 */
+...........>
+#include<iostream>
+
+using namespace std;
+
+class complex{
+	int a,b;
+	public:
+		complex(int , int);
+	    void printnumber(){
+			cout<<"Your number is "<<a<<" + "<<b<<"i"<<endl;
+		}
+};
+complex::complex(int x, int y){//this is a paramaterized constructor
+	a = x;
+	b = y;
+}
+int main(){
+	//implicit call
+	complex c(4,5);
+	//explicit call
+	complex c2 = complex(1,2);
+	c.printnumber();
+	c2.printnumber();
+	return 0;
+}
+........>
+//ca;culating the area of a rectangle with the help of constructors:
+#include<iostream>
+
+using namespace std;
+
+class rectangle{
+	int a, b, c;//three variables are declared for storing lentgh, breadth and area.
+	public:
+		rectangle(int x, int y){//connnstructor is made here with the same name as the class.
+			a = x;
+			b = y;
+		}
+		void calc();//Introducing member function to calculate area.
+		void print();//Introducing another memeber function to print area.
+};
+void rectangle::calc(){
+	c = a *b;//doing the calculations here. it can access all the variables of the class.
+}
+void rectangle::print(){
+	cout<<"Area is "<<c;
+}
+int main(){
+	rectangle c1(5,6);
+	c1.calc();
+	c1.print();
+	return 0;
+}
+..........>
+//we can do the above program by taking inputs from the user as well.
+#include<iostream>
+
+using namespace std;
+
+class rectangle{
+	int a, b, c;
+	public:
+		rectangle(int x, int y){
+			a = x;
+			b = y;
+		}
+		void calc();
+		void print();
+};
+void rectangle::calc(){
+	c = a *b;
+}
+void rectangle::print(){
+	cout<<"Area is "<<c;
+}
+int main(){
+	int a,b;	
+	cout<<"Enter the length and breadth of the rectangle: ";
+	cin>>a>>b;//taking inputs here.
+	rectangle c1 = rectangle(a,b);//remember this step.
+	c1.calc();
+	c1.print();
+	return 0;
+}
