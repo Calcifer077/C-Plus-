52ed902047d0cb8aef29599a17e1fc3a60ed500a
Calcifer077 <115633814+Calcifer077@users.noreply.github.com>
2023-04-24 18:11:19 +0530

    Update Pointers.cpp

--- Pointers.cpp
+++ Pointers.cpp
@@ -1,6 +1,55 @@
 #include<iostream>
 using namespace std;
 
+int main(){
+	int n =5;
+	cout<<&n<<endl;//will print address of the variable.
+	int *ptr = &n;
+	cout<<ptr<<endl;//will print address of the variable.
+	
+	cout<<*ptr<<endl;//Defrencing the pointer. Will print the value of the variable
+	
+	*ptr = 10;
+	
+	cout<<*ptr<<endl;
+	return 0;
+}
+................>
+//VOID PONTER IS THE TYPE OF POINTER WHICH CAN POINT TO A VARIABLE OF ANY DATA TYPE.
+#include<iostream>
+using namespace std;
+
+void printnumber(int* numberptr){
+	cout<<*numberptr<<endl;
+}
+void printletter(char* charptr){
+	cout<<*charptr<<endl;
+}
+void print(void *ptr, char type){
+	switch(type){
+		case 'i':
+			cout<<*((int*)ptr)<<endl;//Casting and derefrencing
+			break;
+		case 'c':
+			cout<<*((char*)ptr)<<endl;
+			break;
+	}
+}
+int main(){
+	int number = 5;
+	//printnumber(&number);
+	char letter = 'a';
+	//printletter(&letter);
+	print(&number, 'i');
+	print(&letter, 'c');
+	
+	return 0;
+}
+
+..............>
+#include<iostream>
+using namespace std;
+
 class A{
 	int a;
 	public:
@@ -19,10 +68,7 @@ int main(){
 	obj.getdata();
 	return 0;
 }
-
-
-
-
+.........>
 #include<iostream>
 using namespace std;
 
