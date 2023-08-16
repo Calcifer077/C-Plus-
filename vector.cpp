cfd543fa6706cd78d484c451a713cc91b3f1c68a
Calcifer077 <115633814+Calcifer077@users.noreply.github.com>
2023-08-16 19:01:46 +0530

    Update vector.cpp

--- vector.cpp
+++ vector.cpp
@@ -53,3 +53,33 @@ int main(){
 	}
 	return 0;	
 }
+.........................................................................................
+.........................................................................................
+.........................................................................................
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+	vector<int> v;
+	v.push_back(1);
+	v.push_back(2);
+	v.push_back(3);
+	
+	vector<int>::iterator it;
+	for(it = v.begin(); it!=v.end();it++){
+		cout<<*it<<endl;
+	}
+	
+	for(auto element:v){
+		cout<<element<<endl;
+	}
+	v.pop_back(); //Will remove the last element.  
+	
+	vector<int> v2 (3,50); //Will create a vector of size 3 and all the elements will be 50.
+	swap(v, v2);//Will swap the two vector.
+	
+	sort(v.begin(), v.end());//Will sort the vector.
+	return 0;	
+}
