86740dade4ef63b1431b5ed2a4f4cc3e98b25956
Calcifer077 <115633814+Calcifer077@users.noreply.github.com>
2023-08-23 22:22:04 +0530

    Update anonymus questions.cpp

--- anonymus questions.cpp
+++ anonymus questions.cpp
@@ -57,3 +57,28 @@ int main(){
 .........................................................................................
 .........................................................................................
 .........................................................................................
+//Program to reverse a string.
+#include<iostream>
+using namespace std;
+
+void swap(string str1, string str2){
+    string temp;
+    temp = str1;
+    str1 = str2;
+    str2 = temp;
+}
+
+int main(){
+    string str = "Hello world!";
+    cout<<str<<endl;
+    int len = str.length();
+    cout<<len<<endl;
+    
+    for(int i = 0; i < len / 2; i++){
+        swap(str[i], str[len - 1 - i]);
+    }
+    for(int i = 0; i< len; i++){
+        cout<<str[i];
+    }
+    return 0;
+}
